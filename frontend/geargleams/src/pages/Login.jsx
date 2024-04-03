2bc83718e33eb869c04139a69bc3dd931883c2c9
abhinendra2904 <abhinendra.yadav21@st.niituniversity.in>
2024-04-04 02:56:35 +0530

    image

--- frontend/geargleams/src/pages/Login.jsx
+++ frontend/geargleams/src/pages/Login.jsx
@@ -1,12 +1,13 @@
-import React from 'react'
+import React from 'react';
 import Login from "../components/Login/Login.jsx";
+import backgroundImage from "../assests/login image.jpg"; // Import your background image here
 
 const LoginPage = () => {
   return (
-    <div>
-      <Login/>
+    <div style={{ backgroundImage: `url(${backgroundImage})`, backgroundSize: "cover", backgroundPosition: "center", height: "100vh" }}>
+      <Login />
     </div>
-  )
-}
+  );
+};
 
-export default LoginPage
+export default LoginPage;
